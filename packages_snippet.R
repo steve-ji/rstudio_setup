0495653567e54f71633e3cc045bff42ddf5f22a8
steve-ji <50185006+steve-ji@users.noreply.github.com>
2023-09-20 13:14:02 -0400

    Create packages_snippet.R

---
+++ packages_snippet.R
@@ -0,0 +1,9 @@
+# List of packages for session
+.packages = c("ggplot2", "plyr", "rms")
+
+# Install CRAN packages (if not already installed)
+.inst <- .packages %in% installed.packages()
+if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst])
+
+# Load packages into session 
+lapply(.packages, require, character.only=TRUE)
